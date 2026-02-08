9e65a8d38f4e78c20466e6b8bf18bf7a0c3aeeae
Ben Wainwright <bwainwright28@gmail.com>
2026-02-08 22:16:39 +0000

    add tests for all of the cases with the motion lights

--- src/tests/lights.test.ts
+++ src/tests/lights.test.ts
@@ -40,11 +40,16 @@ test.each<MotionSensorTestConfig>([
   },
 );
 
-test.each<MotionSensorTestConfig>([
-  ["binary_sensor.bathroom_occupancy", "switch.bathroom_motion_sensor", "bathroom"],
-  ["binary_sensor.living_room_occupancy", "switch.living_room_motion_sensor", "living_room"],
-  ["binary_sensor.hallway_occupancy", "switch.halllway_motion_sensor", "hallway"],
-  ["binary_sensor.bedroom_occupancy", "switch.bedroom_motion_sensor", "bedroom"],
+type MotionSensorTestConfigWithSwitchOff = [
+  occupancySensor: PICK_ENTITY<"binary_sensor">,
+  enableSwitch: PICK_ENTITY<"switch">,
+];
+
+test.each<MotionSensorTestConfigWithSwitchOff>([
+  ["binary_sensor.bathroom_occupancy", "switch.bathroom_motion_sensor"],
+  ["binary_sensor.living_room_occupancy", "switch.living_room_motion_sensor"],
+  ["binary_sensor.hallway_occupancy", "switch.halllway_motion_sensor"],
+  ["binary_sensor.bedroom_occupancy", "switch.bedroom_motion_sensor"],
 ])(
   "when %s switches on and %s is off then no lights turn on",
   async (occupancySensor, enableSwitch) => {
@@ -54,6 +59,41 @@ test.each<MotionSensorTestConfig>([
         mock_assistant.entity.setupState({
           [occupancySensor]: { state: "off" },
           [enableSwitch]: { state: "off" },
+        });
+      })
+      .run(async ({ hass, mock_assistant, lifecycle }) => {
+        lifecycle.onReady(async () => {
+          const turnOnSpy = spyOn(hass.call.light, "turn_on");
+
+          await mock_assistant.entity.emitChange(occupancySensor, {
+            state: "on",
+          });
+
+          expect(turnOnSpy).not.toHaveBeenCalled();
+        });
+      });
+  },
+);
+
+type MotionSensorTestConfigWithBlockingSwitch = [
+  occupancySensor: PICK_ENTITY<"binary_sensor">,
+  enableSwitch: PICK_ENTITY<"switch">,
+  blockingSwitch: PICK_ENTITY<"switch">,
+];
+
+test.each<MotionSensorTestConfigWithBlockingSwitch>([
+  ["binary_sensor.bedroom_occupancy", "switch.bedroom_motion_sensor", "switch.sleep_mode"],
+  ["binary_sensor.living_room_occupancy", "switch.living_room_motion_sensor", "switch.tv_mode"],
+])(
+  "when %s switches on and %s is on but %s is on then no lights turn on",
+  async (occupancySensor, enableSwitch, blockingSwitch) => {
+    await testRunner
+      .bootLibrariesFirst()
+      .setup(async ({ mock_assistant }) => {
+        mock_assistant.entity.setupState({
+          [occupancySensor]: { state: "off" },
+          [enableSwitch]: { state: "on" },
+          [blockingSwitch]: { state: "on" },
         });
       })
       .run(async ({ hass, mock_assistant, lifecycle }) => {
