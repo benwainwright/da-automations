136079d590120d130012509d99c450dc3ddbb1da
Ben Wainwright <bwainwright28@gmail.com>
2026-02-09 19:09:37 +0000

    fixed plant monitor issue

--- src/libraries/bens-flat/services/plants-service.ts
+++ src/libraries/bens-flat/services/plants-service.ts
@@ -3,22 +3,22 @@ import { PICK_ENTITY } from "@digital-alchemy/hass";
 
 export function PlantsService({ hass, bens_flat: { nags }, lifecycle }: TServiceParams) {
   const addPlantAlert = ({ plant }: { plant: PICK_ENTITY<"plant"> }) => {
-    const thePlant = hass.refBy.id(plant);
-    const name = thePlant.attributes.friendly_name;
-    nags.add({
-      trigger: () => thePlant.state === "problem",
-      notification: {
-        message: `Plant ${name} has a problem`,
-        title: `Plant Alert`,
-      },
+    lifecycle.onReady(() => {
+      const thePlant = hass.refBy.id(plant);
+      const name = thePlant.attributes.friendly_name;
+      nags.add({
+        trigger: () => thePlant.state === "problem",
+        notification: {
+          message: `Plant ${name} has a problem`,
+          title: `Plant Alert`,
+        },
+      });
     });
   };
 
-  lifecycle.onReady(() => {
-    addPlantAlert({
-      plant: "plant.marlin",
-    });
-
-    addPlantAlert({ plant: "plant.monroe" });
+  addPlantAlert({
+    plant: "plant.marlin",
   });
+
+  addPlantAlert({ plant: "plant.monroe" });
 }
