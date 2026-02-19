4ec1a218e80499aea8839b3be4875c1ed98ab364
Ben Wainwright <bwainwright28@gmail.com>
2026-02-19 08:28:18 +0000

    remove unnecessary files

--- vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-  test: {
-    globalSetup: "./src/test-helpers/test-setup.ts",
-    globals: true,
-    include: ["./src/**/*"],
-  },
-});
