a65abc549292de3d485a45951caadf0ace589966
Ben Wainwright <bwainwright28@gmail.com>
2026-02-23 20:39:40 +0000

    turn switches back on every day

--- src/libraries/bens-flat/services/scheduler-service/scheduler-service.ts
+++ src/libraries/bens-flat/services/scheduler-service/scheduler-service.ts
@@ -1,6 +1,6 @@
 import { CronExpression, TServiceParams } from "@digital-alchemy/core";
 
-export function SchedulerService({ scheduler, bens_flat: { scene } }: TServiceParams) {
+export function SchedulerService({ scheduler, bens_flat: { scene }, hass }: TServiceParams) {
   const { on, off } = scene.toggle({
     scene: "scene.night_audio",
     snapshot: ["number.bathroom_bass", "number.bedroom_speaker_bass", "number.living_room_bass"],
@@ -14,5 +14,20 @@ export function SchedulerService({ scheduler, bens_flat: { scene } }: TServicePa
   scheduler.cron({
     schedule: CronExpression.EVERY_DAY_AT_9AM,
     exec: off,
+  });
+
+  scheduler.cron({
+    schedule: CronExpression.EVERY_DAY_AT_5AM,
+    exec: () => {
+      hass.call.switch.turn_on({
+        entity_id: [
+          "switch.adaptive_lighting_adapt_brightness_hallway",
+          "switch.adaptive_lighting_adapt_brightness_bedroom",
+          "switch.adaptive_lighting_adapt_brightness_bathroom",
+          "switch.adaptive_lighting_adapt_brightness_living_room",
+          "switch.autoplay_music",
+        ],
+      });
+    },
   });
 }
