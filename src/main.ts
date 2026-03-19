c11e545f52691d70dd5efc80f0d3048bce63b9a3
Ben Wainwright <bwainwright28@gmail.com>
2026-03-19 14:55:54 +0000

    remove heartbeat config

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ void APP.bootstrap({
       LOG_LEVEL: "info",
     },
     synapse: {
-      HEARTBEAT_INTERVAL: 30,
       METADATA_UNIQUE_ID: "551e4cc1-3a68-460e-a3ff-9f70ba752c46",
       ENTITY_CLEANUP_METHOD: "delete",
       REBUILD_ON_ENTITY_CHANGE: true,
