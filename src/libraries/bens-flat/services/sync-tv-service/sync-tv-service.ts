bde21977b7798da6bd53245f12f657b535991100
Ben Wainwright <bwainwright28@gmail.com>
2026-03-19 20:01:38 +0000

    update sync service

--- src/libraries/bens-flat/services/sync-tv-service/sync-tv-service.ts
+++ src/libraries/bens-flat/services/sync-tv-service/sync-tv-service.ts
@@ -1,5 +1,7 @@
 import { TServiceParams } from "@digital-alchemy/core";
 
+const APPLE_TV_SOURCE = "Apple TV";
+
 export function SyncTvService({ hass }: TServiceParams) {
   const appleTv = hass.refBy.id("media_player.apple_tv");
   const tv = hass.refBy.id("media_player.tv");
@@ -10,8 +12,12 @@ export function SyncTvService({ hass }: TServiceParams) {
         entity_id: "button.turn_on_tv",
       });
       await tv.waitForState("on");
-      await tv.select_source({ source: "Apple TV" });
-    } else if (newState.state === "off" && tv.state !== "off") {
+      await tv.select_source({ source: APPLE_TV_SOURCE });
+    } else if (
+      newState.state === "off" &&
+      tv.state !== "off" &&
+      tv.attributes.source === APPLE_TV_SOURCE
+    ) {
       await tv.turn_off();
     }
   });
