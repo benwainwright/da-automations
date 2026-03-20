aa9d0840ff15b0b3912e59327b9178b9e68f5f65
Ben Wainwright <bwainwright28@gmail.com>
2026-03-20 17:56:53 +0000

    tweak boiler service

--- src/libraries/bens-flat/services/boiler-service/boiler-service.ts
+++ src/libraries/bens-flat/services/boiler-service/boiler-service.ts
@@ -2,6 +2,12 @@ import { TServiceParams } from "@digital-alchemy/core";
 import { MDI_HEAT_WAVE, MDI_WATER_BOILER } from "../icons.ts";
 import { RemoveCallback } from "@digital-alchemy/hass";
 
+/**
+ * My boiler has a main element and a boost switch. If they are on on at the same time,
+ * the fuse will blow. This service creates virtual switches that
+ * 1 - Provide the boost timeout behaviour, but also
+ * 2 - Control the underlying switches in a way that ensures they are never on at the same time
+ */
 export function BoilerService({ hass, synapse, context, scheduler }: TServiceParams) {
   const boilerMainElement = hass.refBy.id("switch.boiler_main_element");
   const boilerBoost = hass.refBy.id("switch.boiler_boost_switch");
@@ -33,11 +39,9 @@ export function BoilerService({ hass, synapse, context, scheduler }: TServicePar
     await boilerMainElement.waitForState("off");
     await boilerBoost.turn_on();
     clearBoostOff = scheduler.setTimeout(async () => {
-      await boilerBoost.turn_off();
-      await boilerBoost.waitForState("off");
-      if (boilerSwitch.is_on) {
-        await boilerMainElement.turn_on();
-      }
+      await hass.call.switch.turn_off({
+        entity_id: boost.entity_id,
+      });
     }, [1, "hour"]);
   });
 
