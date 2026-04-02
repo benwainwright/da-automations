c11de77200980727cdb2e15594bc9059388df66f
Ben Wainwright <bwainwright28@gmail.com>
2026-04-02 01:10:36 +0100

    add nage mode switch

--- src/libraries/bens-flat/services/nag-service/nag-service.ts
+++ src/libraries/bens-flat/services/nag-service/nag-service.ts
@@ -1,13 +1,19 @@
 import { TServiceParams } from "@digital-alchemy/core";
 import { NotifyConfig } from "../notification/notification.ts";
 
-export function NagService({ bens_flat: { notify }, scheduler }: TServiceParams) {
+export function NagService({ bens_flat: { notify }, scheduler, synapse, context }: TServiceParams) {
+  const nagMode = synapse.switch({
+    context,
+    suggested_object_id: "nag_mode",
+    name: "Nag Mode",
+  });
   const nags: {
     callback: () => boolean | Promise<boolean>;
     message: NotifyConfig;
   }[] = [];
 
   scheduler.setInterval(async () => {
+    if (!nagMode.is_on) return;
     for (const nag of nags) {
       const { callback, message } = nag;
       const doNag = await callback();
