7ac754ec5169781508e7013e6bd91012d4d66323
Ben Wainwright <bwainwright28@gmail.com>
2026-05-04 14:46:35 +0100

    correctly play latest podcast entry

--- src/libraries/bens-flat/services/podcasts/podcast-service.ts
+++ src/libraries/bens-flat/services/podcasts/podcast-service.ts
@@ -1,22 +1,60 @@
 import { TServiceParams } from "@digital-alchemy/core";
 
-export function PodcastService({ hass, synapse, context }: TServiceParams) {
+export function PodcastService({
+  hass,
+  synapse,
+  context,
+  bens_flat: { mediaPlayer, entityIds },
+}: TServiceParams) {
   const getPod = synapse.button({
-    name: "get pod episodes",
+    name: "Play latest news podcast",
     context,
   });
 
+  interface GetPodcastEpisodesResponse {
+    episodes: {
+      media_title: string;
+      media_content_id: string;
+      duration: number;
+      media_image: string;
+      favourite: boolean;
+      release_date: string;
+    }[];
+  }
+
   const getLatestEpisode = async (podcastUrl: string) => {
-    const results = await hass.call.mass_queue.get_podcast_episodes({
+    const results: GetPodcastEpisodesResponse = await hass.call.mass_queue.get_podcast_episodes({
       /* cspell:disable-next-line */
       config_entry_id: "01KM5ZV4PJ58R66HNJFD7V5QRT",
       uri: podcastUrl,
     });
 
-    console.log(JSON.stringify(results, null, 2));
+    const [first] = results.episodes.toSorted((a, b) =>
+      new Date(a.release_date) > new Date(b.release_date) ? -1 : 1,
+    );
+
+    return first;
+  };
+
+  const playLatestEpisode = async (podcastUri: string) => {
+    const latestEpisode = await getLatestEpisode(podcastUri);
+
+    await mediaPlayer.play({
+      player: [
+        entityIds.mediaPlayers.hallway,
+        entityIds.mediaPlayers.bathroom,
+        entityIds.mediaPlayers.bedroom,
+        entityIds.mediaPlayers.livingRoom,
+      ],
+      id: latestEpisode.media_content_id,
+      type: "music",
+      volume: 0.5,
+    });
   };
 
   getPod.onPress(async () => {
-    await getLatestEpisode("library://podcast/3");
+    await playLatestEpisode("library://podcast/3");
   });
+
+  return { playLatestEpisode };
 }
